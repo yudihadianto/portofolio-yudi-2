1dbe2dc4325cb3b934b252cd302115e09d04ca2e
Yudi Hadianto <yudihadiyanto7@gmail.com>
2021-12-17 02:48:57 +0700

    a

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault();
     let name = document.getElementById("name").value;
     let messege = document.getElementById("messege").value;
-    var win = window.open(`https://wa.me/+918450996596?text=Hi%20I%27m%20${name},%20${messege}`, '_blank');
+    var win = window.open(
+      `https://wa.me/+6281286291383?text=Hallo%20Yudi...%20${name},%20${messege}`,
+      "_blank"
+    );
 });
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
